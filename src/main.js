6e1e548d830b1e5ca4b1dd75087a04d0ad32fea1
wangkemiao <wangkemiao@baidu.com>
2015-04-23 19:04:06 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,19 @@
+/**
+ * @file page components using k-component
+ *
+ * @author Leo Wang(leowang721@gmail.com)
+ */
+
+define(function (require) {
+
+    /**
+     * page components using k-component
+     * @class component.page
+     * @type {Object}
+     */
+    var pageComponents = {
+        version: '0.0.1-alpha.1'
+    };
+
+    return pageComponents;
+});
